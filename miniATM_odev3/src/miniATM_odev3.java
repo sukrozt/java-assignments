a522b3f019740561292198a0eb8c01d6ac85aca3
Şükriye Öztürk <sukriyeo13@gmail.com>
2023-02-20 19:49:24 +0300

    add comment section

--- miniATM_odev3/src/miniATM_odev3.java
+++ miniATM_odev3/src/miniATM_odev3.java
@@ -1,3 +1,4 @@
+//bu kod küçük bir atm uygulaması. para çekme, para yatırma, bakiye görüntüleme olmak üzere 3 fonksiyonu, 1 adet de çıkış komutu var.
 import java.util.Scanner;
 
 public class miniATM_odev3 {
@@ -7,13 +8,13 @@ public class miniATM_odev3 {
             int ilk_bakiye = 5000;
             Scanner scan = new Scanner(System.in);
             System.out.println("\nYapmak istediğiniz işlemi giriniz: \n1 : bakiye görüntüleme\n2 : para çekme\n3 : para yatırma\n4 : sistemden çıkış");
-            int input_user = scan.nextInt();
+            int input_user = scan.nextInt(); //kullanıcıdan hangi işlemin yapılacağı alınıyor
             switch(input_user){
-                case 1:{
+                case 1:{ //1: bakiye görüntüleme
                     System.out.print("\nMevcut bakiyeniz: " + ilk_bakiye + "TL\n");
                     break;
                 }
-                case 2:{
+                case 2:{ //2: para çekme
                     System.out.print("\nÇekmek istediğiniz tutarı giriniz: ");
                     int cekilen_para = scan.nextInt();
                     if (cekilen_para > ilk_bakiye){
@@ -24,18 +25,18 @@ public class miniATM_odev3 {
                     }
                     break;
                 }
-                case 3:{
+                case 3:{ //3: para yatırma
                     System.out.print("\nYatırmak istediğiniz tutarı giriniz: ");
                     int yatirilan_para = scan.nextInt();
                     System.out.println("\nİşlem başarılı");
                     break;
                 }
-                case 4:{
+                case 4:{ //4: çıkış yapma. sistemden çıkış yapılana kadar sistem tekrardan hangi işlemin yapılacağını sorar.
                     System.out.print("\nSistemden çıkış yapıldı. İyi günler dileriz.\n");
-                    is_this_exit = false;
+                    is_this_exit = false; //is_this_exit boolean değişkeni sayesinde sistemden çıkış yapılıp yapılmadığını anlıyorum
                     break;
                 }
-                default :{
+                default :{ //1-2-3-4 komutları dışında bir karakter girildiğinde uygulama tekrar komut sorar.
                     System.out.print("\nGeçersiz komut girdiniz. Lütfen tekrar deneyin.\n");
                 }
             }
