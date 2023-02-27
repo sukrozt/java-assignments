1f6923331ac3951b87ab9038002e03370929b203
Şükriye Öztürk <sukriyeo13@gmail.com>
2023-02-27 13:24:53 +0300

    added comments

--- odev4_ogrenciVeritabani/src/Main.java
+++ odev4_ogrenciVeritabani/src/Main.java
@@ -1,3 +1,4 @@
+//bu kod verileri sağlanan öğrencinin sınıfını belirleyip gerekli bilgilerin çıktısını verir
 import java.util.Scanner;
 
 public class Main {
@@ -5,14 +6,14 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Kaç öğrenci kaydedilmesini istiyorsunuz?: ");
-        int studentNumber = scan.nextInt();
+        int studentNumber = scan.nextInt(); //kullanıcıdan kodun kaç kez çalıştırılacağı verisi alınır
 
         for(int i = 0; i < studentNumber; i++){
             System.out.println((i+1) + ". öğrencinin sınıfı nedir?: \n1: Genel öğrenci\n2: Lise öğrencisi\n3: Üniversite öğrencisi");
-            int studentClass = scan.nextInt();
+            int studentClass = scan.nextInt(); //kullanıcıdan hangi case'e gidileceği ve dolayısıyla hangi sınıftan obje oluşturulacağı bilgisi alınır
             switch (studentClass) {
                 case 1: {
-                    System.out.println("Öğrencinin adı:");
+                    System.out.println("Öğrencinin adı:"); //eğer öğrenci 'student' sınfına aitse öğrenci ismi ve notu sağlanır
                     String name = scan.next();
                     System.out.println("Öğrencinin not ortalaması:");
                     double averageGrade = scan.nextDouble();
@@ -21,7 +22,7 @@ public class Main {
                     break;
                 }
                 case 2: {
-                    System.out.println("Öğrencinin adı:");
+                    System.out.println("Öğrencinin adı:"); //eğer öğrenci 'HighschoolStudent' sınfına aitse öğrenci ismi, notu ve alacağı belgenin türü sağlanır
                     String name = scan.next();
                     System.out.println("Öğrencinin not ortalaması:");
                     double averageGrade = scan.nextDouble();
@@ -30,7 +31,7 @@ public class Main {
                     break;
                 }
                 case 3: {
-                    System.out.println("Öğrencinin adı:");
+                    System.out.println("Öğrencinin adı:"); //eğer öğrenci 'CollegeStudent' sınfına aitse öğrenci ismi, aldığı dersin notu ve kredisi sağlanır
                     String name = scan.next();
                     System.out.println("Öğrencinin not ortalaması:");
                     double averageGrade = scan.nextDouble();
